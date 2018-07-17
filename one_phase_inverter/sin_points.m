1bcfbe90f26f009da7878eda71381d960151d4d4
r0n4n <ronan.blanchard35@gmail.com>
2018-07-17 18:50:47 +0200

    change ref magnitude

--- one_phase_inverter/sin_points.m
+++ one_phase_inverter/sin_points.m
@@ -3,19 +3,19 @@ close all
 
 N= 250 ; % nbr of points 
 fn=(2*pi)/N ; % the "sample frequency"
-A = 512 ; % the magnitude 
+A = 2000000 ; % the magnitude 
 voltage_sf = 0.67925 ; % the scale factor of the software for the ADC 
 offset = 0 ; %1023 ; % offset the signal 
 
 sin_array = zeros(1,N) ; % init the array 
 
-% for i=1:N
-%     sin_array(i) = round((A*sin(fn*i)+offset)*voltage_sf)  ;  % we use "round" because it is an array of int 
-% end 
-
 for i=1:N
-    sin_array(i) =1/2.02*sin(fn*i)  ;  % we use "round" because it is an array of int 
-end
+    sin_array(i) = round(A*sin(fn*i))  ;  % we use "round" because it is an array of int 
+end 
+
+% for i=1:N
+%     sin_array(i) =1/2.02*sin(fn*i)  ;  % we use "round" because it is an array of int 
+% end
 
 %% plot 
 
